91c0b17393b27b883b009d76816ed0cd7dd31703
Igor Franca <m.igorfranca@gmail.com>
2024-01-30 21:25:55 -0300

    Create Avatar Component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,4 @@
-import { Comment } from './Comment';
+import {Avatar} from './Avatar';
 import {Comment} from './Comment';
 import styles from './Post.module.css';
 
@@ -7,7 +7,7 @@ export function Post(){
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img  className={styles.avatar} src='https://github.com/igor-franca.png' />
+                    <Avatar src='https://github.com/igor-franca.png' />
                     <div className={styles.authorInfo}>
                         <strong>Diogo Fernandes</strong>
                         <span>web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import {PencilLine} from 'phosphor-react';
-import { PencilLine } from 'phosphor-react';
+import {Avatar} from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,6 +11,7 @@ export function Sidebar() {
                 src='https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50'
             />
             <div className={styles.profile}>
+                <Avatar src="https://github.com/igor-franca.png" />
 
                 <strong>Igor Franca</strong>
                 <span>Web Developer</span>
